84575a73582d4c7e6749c0f31c989d1dced01d13
Alex Nazimok <anazimok@laureate.net>
2013-04-06 13:49:03 -0400

    Initial commit

---
+++ main.php
@@ -0,0 +1,55 @@
+<?php
+require_once("workflows.php");
+
+$CMD_PATH="/usr/local/bin/task";
+
+$alias = array(
+    "ls" => "pending",
+    "comp" => "completed",
+    "done" => "completed",
+    "completed" => "completed"
+);
+
+$q = trim($argv[1]);
+$wf = new Workflows();
+
+$output = "[" . shell_exec($CMD_PATH . " export status:".$alias[$q]) . "]";
+
+$json = json_decode($output);
+
+foreach($json as $task) {
+
+    $wf->result(
+        $task->id,
+        $task->uuid,
+        $task->description,
+        buildSubtitle($task),
+        'icon.png'
+    );
+}
+
+echo $wf->toxml();
+
+
+function buildSubtitle($task) {
+    $start = date_parse($task->entry);
+    $end = date_parse($task->end);
+
+    $startDate = sprintf("Entered On: %s/%s/%s",
+        $start['month'],
+        $start['day'],
+        $start['year']);
+
+    $endDate = sprintf("     Completed On: %s/%s/%s",
+        $end['month'],
+        $end['day'],
+        $end['year']);
+
+    if ($end['year'] == NULL) {
+        return $startDate . "    Press Cmd + Enter to complete the task";
+    } else {
+        return $startDate . $endDate;
+    }
+}
+
+?>
